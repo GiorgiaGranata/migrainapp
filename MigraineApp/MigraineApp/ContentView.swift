9349afa43860fadf0e8d170a19c27dc199964b55
Giorgia <giorgiagranata9@gmail.com>
2024-12-09 12:11:53 +0100

    lista con giorni divisi per mese, deve essere migliorata

---
+++ MigraineApp/MigraineApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MigraineApp
+//
+//  Created by Giorgia Granata on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MigraineApp/MigraineApp/MigraineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MigraineAppApp.swift
+//  MigraineApp
+//
+//  Created by Giorgia Granata on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MigraineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
